a67731c5c92af9b3d08c93c118bfe249503af16c
Marco-Zanoni <marco2.zanoni@mail.polimi.it>
2020-03-19 19:38:44 +0100

    Parzialmente aggiunto regole degli Dei

--- src/main/java/it/polimi/ingsw/rules/GodRules.java
+++ src/main/java/it/polimi/ingsw/rules/GodRules.java
@@ -17,15 +17,15 @@ public abstract class GodRules {
     public void consentBuild(Worker worker, Position position) throws CantBuildException {
         if(worker.getPosition().getDistanceFrom(position)!=1 || !askEnemyConsent())
         {
-            throw new OutOfReachException("U went too far");
+            throw new OutOfReachException("Maximum builing limit excedeed");
         }
         else if(!askEnemyConsent() || Board.getTile(position).hasDome())
         {
-            throw new AlreadyCompleteException("Dome");
+            throw new AlreadyCompleteException("This tile is no longer available for building");
         }
         else if(!askEnemyConsent() || position.getWorker()!=null)
         {
-            throw new AlreadyOccupiedException("There is another");
+            throw new AlreadyOccupiedException("This tile is alrealy occupied");
         }
 
     }
@@ -33,26 +33,27 @@ public abstract class GodRules {
     public void consentMovement(Worker worker, Position position) throws CantMoveException {
         if(worker.getPosition().getDistanceFrom(position)!=1 || !askEnemyConsent())
         {
-            throw new aException("U went too far");
+            throw new aException("Maximum moviment limit excedeed");
         }
         else if(!askEnemyConsent() || position.getWorker()!=null)
         {
-            throw new bException("There is another");
+            throw new bException("This tile is alrealy occupied");
         }
         else if(!askEnemyConsent() || Board.getTile(position).hasDome())
         {
-            throw new cException("Dome");
+            throw new cException("This tile is no longer available for movement");
         }
         else if(!askEnemyConsent() || Board.getTile(position).getHeight() > Board.getTile(worker.getPosition()).getHeight() +1)
         {
-            throw new dException("U went too high");
+            throw new dException("Unreachable tile from this position");
         }
 
 
     }
 
-    public boolean checkWin(Worker worker) {
-        return(Board.getTile(worker.getPosition()).getHeight()==TOP);
+    public boolean isWinner(Worker worker, Position position) {
+        return(Board.getTile(worker.getPosition()).getHeight()==MID && Board.getTile(position).getHeight()==TOP); // return(Board.getTile(worker.getPosition()).getHeight()==TOP);
+                                                                                                                    // Dipende quando viene effettuato il controllo, adesso subito successivo al consentMovement
     }
 
     public boolean askEnemyConsent() { return true; } // Qui si chiede consenso alle regole dei nemici
