9d8b0f97a9dc97598ad45d20e7f21491be9cf0fb
Stefano-Vanerio <61703922+Stefano-Vanerio@users.noreply.github.com>
2020-03-21 09:13:01 +0100

    Aggiunta dei metodi dei God, dei metodi di winner

--- src/main/java/it/polimi/ingsw/model/Session.java
+++ src/main/java/it/polimi/ingsw/model/Session.java
@@ -1,12 +1,17 @@
 package it.polimi.ingsw.model;
+import it.polimi.ingsw.enumerations.Gods;
+import it.polimi.ingsw.model.map.Board;
 import it.polimi.ingsw.model.player.Player;
+import it.polimi.ingsw.model.player.Worker;
 
 import java.util.ArrayList;
 
-// STEFANO
+
 
 public class Session {
     private static ArrayList<Player> players = new ArrayList<>();
+    private static Board board;
+    private static ArrayList<Gods> godsList;
 
     public static void addPlayer(Player player) {
         players.add(player);
@@ -18,6 +23,27 @@ public class Session {
         return (ArrayList<Player>) players.clone();
     }
 
+    public static void addGod(Gods gods) {
+        godsList.add(gods);
+    }
 
+    // Returns shallow copy of gods
+    @SuppressWarnings("unchecked")
+    public static ArrayList<Gods> getGods() {
+        return (ArrayList<Gods>) godsList.clone();
+    }
 
-}
+    public static void startLobby() {
+        //da riempire quando avremo controller
+    }
+
+    public static boolean hasWinner(){
+            for(Player player : players) {
+                    if(player.isWinner()) {
+                        return true;
+                    }
+                }
+            return false;
+        }
+    }
+
