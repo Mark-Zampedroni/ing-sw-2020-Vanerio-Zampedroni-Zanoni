a97c588f53d7a035dd846620c8274ff9ae3eafd3
Stefano-Vanerio <61703922+Stefano-Vanerio@users.noreply.github.com>
2020-03-21 16:44:50 +0100

    Static in board

--- src/main/java/it/polimi/ingsw/model/map/Board.java
+++ src/main/java/it/polimi/ingsw/model/map/Board.java
@@ -11,17 +11,9 @@ public class Board {
     private final static Tile[][] tiles = new Tile[Board.WIDTH][Board.HEIGHT];
 
     static {
-        for (int i=0; i<5; i++) {
-            for (int j=0; i<5; i++) {
+        for (int i=0; i<Board.WIDTH; i++) {
+            for (int j=0; i<Board.HEIGHT; i++) {
                 tiles [i] [j] = new Tile();
-            }
-        }
-    }
-
-    public static void resetBoard() {
-        for(int i = 0; i < Board.WIDTH; i++) {
-            for(int j = 0; j < Board.HEIGHT; j++) {
-                tiles[i][j] = new Tile();
             }
         }
     }
