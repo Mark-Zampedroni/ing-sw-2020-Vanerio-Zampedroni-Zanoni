64a56fcbfc3110b91d0fd1dc0022a5de9d88f491
Marco-Zanoni <marco2.zanoni@mail.polimi.it>
2020-03-30 00:33:01 +0200

    It's all consenting together

--- src/main/java/it/polimi/ingsw/rules/GodSharedRules.java
+++ src/main/java/it/polimi/ingsw/rules/GodSharedRules.java
@@ -8,30 +8,31 @@ import it.polimi.ingsw.model.player.Position;
 import it.polimi.ingsw.model.player.Worker;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import static it.polimi.ingsw.constants.Height.MID;
 import static it.polimi.ingsw.constants.Height.TOP;
 
 public abstract class GodSharedRules {
 
-    public boolean flag=false;
+    public boolean flag = false;
 
-    public ArrayList<ActionType> fixOthers(Position position, Position oldPosition, Worker worker){
+    public ArrayList<ActionType> fixOthers(Position position, Position oldPosition, Worker worker) {
         ArrayList<ActionType> actions = new ArrayList<>();
         actions.add(ActionType.BUILD);
         return actions;
     }
 
     public ArrayList<ActionType> executeBuild(Position position, Worker worker) {
-        ArrayList <ActionType> nextAction = new ArrayList<>();
+        ArrayList<ActionType> nextAction = new ArrayList<>();
         Tile tile = Board.getTile(position);
         tile.increaseHeight();
         nextAction.add(ActionType.END_TURN);
         return nextAction;
-     }
+    }
 
     public void consentBuild(Worker worker, Position position) throws CantActException {
-        Check.distance(worker,position);
+        Check.distance(worker, position);
         Check.dome(position);
     }
 
@@ -41,13 +42,46 @@ public abstract class GodSharedRules {
         Check.height(worker, position);
         // CHECK ATHENA BLOCK
     }
+
     public boolean isWinner(Worker worker, Position position) {
-        return(Board.getTile(worker.getPosition()).getHeight()==MID && Board.getTile(position).getHeight()==TOP);
+        return (Board.getTile(worker.getPosition()).getHeight() == MID && Board.getTile(position).getHeight() == TOP);
     }
 
-    public boolean blockedByEnemy(Worker worker, Position position) { return false; }
-}
+    public boolean blockedByEnemy(Worker worker, Position position) {
+        return false;
+    }
 
+
+    public boolean consentSelection(Worker worker, List<ActionType> actions) {
+        int i, j;
+        for (ActionType action : actions) {
+            for (i = -1; i < 2; i++) {
+                for (j = -1; j < 2; j++) {
+                    try {
+                        switch (action) {
+                            case BUILD:
+                                consentBuild(worker, new Position(worker.getPosition().getX() + i, worker.getPosition().getY() + j));
+                            case MOVE:
+                                consentMovement(worker, new Position(worker.getPosition().getX() + i, worker.getPosition().getY() + j));
+                                return true;
+                        }
+                    } catch (CantActException e){}
+                }
+            }
+            return false;}
+    return false;}
+
+
+
+
+
+
+
+
+
+
+
+}
         /*
         public ArrayList<ActionType> buildTowerOn (Position position, Worker worker) {
             GodSharedRules rules = worker.getMaster().getRules();
