df44060baa626502f62dcf9eea2f786021961ab8
Stefano-Vanerio <61703922+Stefano-Vanerio@users.noreply.github.com>
2020-04-03 22:26:40 +0200

    Testing di Demeter, Hephaestus, Hestia, Atlas

--- src/test/java/it/polimi/ingsw/rules/AtlasRulesTest.java
+++ src/test/java/it/polimi/ingsw/rules/AtlasRulesTest.java
@@ -3,26 +3,29 @@ package it.polimi.ingsw.rules;
 import it.polimi.ingsw.enumerations.Action;
 import it.polimi.ingsw.exceptions.actions.CantActException;
 import it.polimi.ingsw.model.Session;
+import it.polimi.ingsw.model.map.Board;
 import it.polimi.ingsw.model.player.Player;
 import it.polimi.ingsw.model.map.Position;
 import it.polimi.ingsw.model.player.Worker;
-import it.polimi.ingsw.rules.gods.HephaestusRules;
+import it.polimi.ingsw.rules.gods.AtlasRules;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
-
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class HephaestusRulesTest {
+class AtlasRulesTest {
+
     Player player;
     Worker worker;
-    HephaestusRules test = new HephaestusRules();
+    AtlasRules test= new AtlasRules();
 
     @BeforeEach
-    void setUp() {
+    void setUp(){
         Session.getBoard().clear();
         player = new Player("TestName");
         Session.addPlayer(player);
@@ -36,35 +39,16 @@ class HephaestusRulesTest {
         Session.removePlayer(player);
     }
 
-
-    @Test
-    void consentBuild() {
-        worker.setPosition(1,1);
-        assertDoesNotThrow(()->test.consentBuild(worker, new Position(2,2)));
-        test.setEvent(true);
-        test.setPos(new Position(2,2));
-        assertThrows(CantActException.class, ()->test.consentBuild(worker, new Position(2,1)));
-    }
-
     @Test
     void executeBuild() {
         Position position = new Position(1,2);
         test.setEvent(false);
         test.executeBuild(position);
-        assertTrue(test.getEvent());
-        assertTrue(position.equals(test.getPos()));
-        assertEquals(Session.getBoard().getTile(position).getHeight(),1);
-    }
-
-    @Test
-    void afterBuild(){
-        List<Action> list = test.afterBuild();
-        assertEquals(list.get(0), Action.END_TURN);
-        assertEquals(list.get(1), Action.BUILD);
-        assertEquals(list.size(),2);
+        assertEquals(Session.getBoard().getTile(position).getHeight(), 1);
+        assertFalse(Session.getBoard().getTile(position).hasDome());
         test.setEvent(true);
-        list= test.afterBuild();
-        assertEquals(list.get(0), Action.END_TURN);
-        assertEquals(list.size(), 1);
+        position = new Position (3,4);
+        test.executeBuild(position);
+        assertTrue(Session.getBoard().getTile(position).hasDome());
     }
 }
