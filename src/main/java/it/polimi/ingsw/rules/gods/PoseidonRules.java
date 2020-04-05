a6d683f5631256b5222101f677bca4deef8e1456
Stefano-Vanerio <61703922+Stefano-Vanerio@users.noreply.github.com>
2020-04-05 09:55:52 +0200

    Ottimizzazione di Poseidon, ora eredita da EventRules

--- src/main/java/it/polimi/ingsw/rules/gods/PoseidonRules.java
+++ src/main/java/it/polimi/ingsw/rules/gods/PoseidonRules.java
@@ -9,90 +9,82 @@ import it.polimi.ingsw.model.player.Player;
 import it.polimi.ingsw.model.player.Worker;
 import it.polimi.ingsw.rules.Check;
 import it.polimi.ingsw.rules.CommonRules;
+import it.polimi.ingsw.rules.EventRules;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class PoseidonRules extends CommonRules {
+public class PoseidonRules extends EventRules {
     //Il controllo sull'altezza della tile del worker che non si è mosso viene effettuato come ritorno
     private int counter;
     private Worker movedWorker;
-    private Player master;
     private Worker unmovedWorker;
 
-    private boolean multiBuild=false;
+    public void setMovedWorker(Worker worker) {
+        this.movedWorker = worker;
+    }
 
-    public void setMultiBuild (boolean state) {multiBuild=state;}
-    public boolean getMultiBuild () { return multiBuild;}
-
-    public void setMaster(Player master) {this.master=master;}
-    public Player getMaster() {return master;}
-
-    public void setMovedWorker(Worker worker) {this.movedWorker=worker;}
-    public Worker getMovedWorker() {return movedWorker;}
+    public Worker getMovedWorker() {
+        return movedWorker;
+    }
 
     public int getCounter() {
         return counter;
     }
 
-    public void increaseCounter(){
+    public void increaseCounter() {
         counter++;
     }
 
-    public void clearCounter(){
-        counter=0;
+    public void clearCounter() {
+        counter = 0;
     }
 
-    public void setUnmovedWorker(){
-        if (master.getWorkers().get(0) != getMovedWorker()){
-            unmovedWorker = getMaster().getWorkers().get(0);
+    public void setUnmovedWorker() {
+        if (movedWorker.getMaster().getWorkers().get(0) != getMovedWorker()) {
+            unmovedWorker = movedWorker.getMaster().getWorkers().get(0);
         } else {
-            unmovedWorker= getMaster().getWorkers().get(1);
+            unmovedWorker = movedWorker.getMaster().getWorkers().get(1);
         }
     }
 
-    public Worker getUnmovedWorker() {return unmovedWorker;}
+    public Worker getUnmovedWorker() {
+        return unmovedWorker;
+    }
 
     @Override
     public void executeMove(Worker worker, Position position) {
-    super.executeMove(worker, position);
-    setMaster(worker.getMaster());
-    setMovedWorker(worker);
-    setUnmovedWorker();
+        super.executeMove(worker, position);
+        setMovedWorker(worker);
+        setUnmovedWorker();
     }
 
     @Override
-    public void executeBuild(Position position) {
-        if (!multiBuild) {
-        super.executeBuild(position);
-        clearCounter();
-        if (Session.getBoard().getTile(getUnmovedWorker().getPosition()).getHeight() == 0) {
-            setMultiBuild(true);}
-        } else {
-            if (getCounter()<3) {
-                increaseCounter();
-                super.executeBuild(position);
+    public void consentSelect(Worker worker) throws CantActException{
+        if (getEvent()) {
+        if (! worker.equals(unmovedWorker)) {
+            throw new CantActException("This worker can't do any action");
+        }}
+        super.consentSelect(worker);
+    }
+
+    @Override
+    public List<Action> afterBuild() {
+        List<Action> list = super.afterBuild();
+        if (!getEvent()) {
+            if (Session.getBoard().getTile(getUnmovedWorker().getPosition()).getHeight() == 0) {
+                setEvent(true);
+                list.add(Action.SELECT_WORKER);}
+                return list;
+            } else {
+                if (getCounter() < 2) {
+                    increaseCounter();
+                    list.add(Action.BUILD);
+                    return list;
+                } else {
+                    clearCounter();
+                    return list;
+                }
             }
         }
     }
-
-    @Override
-    public void consentBuild(Worker worker, Position position) throws CantActException {
-        if (multiBuild) {
-            if (worker.equals(unmovedWorker)) {
-                super.consentBuild(unmovedWorker, position);
-                return;
-            } else { throw new CantActException("Wrong Player for final build");}
-        } else {super.consentBuild(worker,position);}
-    }
-
-    @Override
-    public List<Action> afterBuild(){
-        List<Action> list = super.afterBuild();
-        if (!multiBuild) {
-        return list; } else {
-            if (getCounter() < 3) {
-                list.add(Action.BUILD);
-                return list;
-            } else { return list; } } }
-}
--- src/test/java/it/polimi/ingsw/rules/PoseidonRulesTest.java
+++ src/test/java/it/polimi/ingsw/rules/PoseidonRulesTest.java
@@ -43,12 +43,10 @@ class PoseidonRulesTest {
     void setUnmovedWorker() {
         Worker worker2 = player.getWorkers().get(1);
         test.setMovedWorker(worker);
-        test.setMaster(player);
         test.setUnmovedWorker();
         assertEquals(test.getUnmovedWorker(),worker2);
         assertEquals(test.getMovedWorker(), worker);
         test.setMovedWorker(worker2);
-        test.setMaster(player);
         test.setUnmovedWorker();
         assertEquals(test.getUnmovedWorker(),worker);
         assertEquals(test.getMovedWorker(), worker2);
@@ -60,69 +58,68 @@ class PoseidonRulesTest {
         worker.setPosition(1,1);
         test.executeMove(worker, new Position(1,2));
         assertTrue(worker.getPosition().equals(new Position(1,2)));
-        assertEquals(test.getUnmovedWorker(),worker2);
+        assertEquals(test.getUnmovedWorker(), worker2);
         assertEquals(test.getMovedWorker(), worker);
-        assertEquals(worker.getMaster(), test.getMaster());
     }
 
     @Test
-    void consentMovement() {
+    void consentSelect() {
         Worker worker1 = player.getWorkers().get(1);
         worker.setPosition(new Position(2,2));
         worker1.setPosition(3,4);
-        test.setMultiBuild(false);
-        assertDoesNotThrow(()->test.consentBuild(worker, new Position(3,2)));
-        test.setMultiBuild(true);
-        test.setMovedWorker(worker);
-        test.setMaster(player);
-        test.setUnmovedWorker();
-        assertThrows(CantActException.class, ()->test.consentBuild(worker, new Position(3,3)));
-        assertDoesNotThrow(()->test.consentBuild(worker1, new Position(3,3)));
-    }
-
-    @Test
-    void executeBuild() {
-        worker.setPosition(1,1);
-        Position position = new Position(1,2);
-        test.setMultiBuild(false);
-        test.setMaster(player);
+        test.setEvent(false);
+        assertDoesNotThrow(()->test.consentSelect(worker));
+        test.setEvent(true);
         test.setMovedWorker(worker);
         test.setUnmovedWorker();
-        test.executeBuild(position);
-        assertTrue(test.getMultiBuild());
-        assertEquals(Session.getBoard().getTile(position).getHeight(),1);
-        test.executeBuild(position);
-        assertEquals(test.getCounter(),1);
-        assertEquals(Session.getBoard().getTile(position).getHeight(),2);
-        test.executeBuild(position);
-        assertEquals(test.getCounter(),2);
-        assertEquals(Session.getBoard().getTile(position).getHeight(),3);
-        test.setMultiBuild(false);
-        test.clearCounter();
-        assertEquals(test.getCounter(),0);
-        test.executeBuild(position);
-        assertTrue(Session.getBoard().getTile(position).hasDome());
-
+        assertThrows(CantActException.class, ()->test.consentSelect(worker));
+        assertDoesNotThrow(()->test.consentSelect(worker1));
     }
 
     @Test
     void afterBuild(){
-        test.setMultiBuild(false);
-        List<Action> list = test.afterBuild();
+
+        //not correct position for second worker and multibuild
+        Position position = new Position(3,3);
+        Session.getBoard().getTile(position).increaseHeight();
+        test.executeMove(worker, new Position(2,3));
+        test.getUnmovedWorker().setPosition(position);
+        test.setEvent(false);
+        List<Action> list =  test.afterBuild();
         assertEquals(list.get(0), Action.END_TURN);
         assertEquals(list.size(),1);
-        test.setMultiBuild(true);
+        assertFalse(test.getEvent());
+
+        //correct position for multibuild, firstbuild
+        position = new Position(2,2);
+        test.getUnmovedWorker().setPosition(position);
         test.clearCounter();
+        list= test.afterBuild();
+        assertEquals(list.get(0), Action.END_TURN);
+        assertEquals(list.get(1), Action.SELECT_WORKER);
+        assertEquals(list.size(), 2);
+        assertTrue(test.getEvent());
+        assertEquals(test.getCounter(),0);
+
         list= test.afterBuild();
         assertEquals(list.get(0), Action.END_TURN);
         assertEquals(list.get(1), Action.BUILD);
         assertEquals(list.size(), 2);
-        test.increaseCounter();
-        test.increaseCounter();
-        test.increaseCounter();
+        assertTrue(test.getEvent());
+        assertEquals(test.getCounter(),1);
+
+        list= test.afterBuild();
+        assertEquals(list.get(0), Action.END_TURN);
+        assertEquals(list.get(1), Action.BUILD);
+        assertEquals(list.size(), 2);
+        assertTrue(test.getEvent());
+        assertEquals(test.getCounter(),2);
+
         list = test.afterBuild();
         assertEquals(list.get(0), Action.END_TURN);
         assertEquals(list.size(),1);
+        assertTrue(test.getEvent());
+        assertEquals(test.getCounter(),0);
     }
 
 }
