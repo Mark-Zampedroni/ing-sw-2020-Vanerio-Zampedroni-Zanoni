cba32e71e5ed131f02d4e0abc1297410a9064d0f
Mark Zampedroni <10608097@polimi.it>
2020-04-06 17:32:37 +0200

    Ridotto i costruttori

--- src/main/java/it/polimi/ingsw/exceptions/utility/NotInstantiableClass.java
+++ src/main/java/it/polimi/ingsw/exceptions/utility/NotInstantiableClass.java
@@ -1,6 +1,5 @@
 package it.polimi.ingsw.exceptions.utility;
 
 public class NotInstantiableClass extends Exception {
-    public NotInstantiableClass(String message) { super(message); }
     public NotInstantiableClass() { super("This class can't have instances"); }
 }
