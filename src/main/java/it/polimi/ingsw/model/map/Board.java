c8a9bd1b12165eaaa1fcbd29eb83eb10fc81b342
Stefano-Vanerio <61703922+Stefano-Vanerio@users.noreply.github.com>
2020-04-10 10:47:23 +0200

    Commenti per Javadoc per Board

--- src/main/java/it/polimi/ingsw/model/map/Board.java
+++ src/main/java/it/polimi/ingsw/model/map/Board.java
@@ -1,6 +1,9 @@
 package it.polimi.ingsw.model.map;
 
-
+/**
+ * Game map, contains 25 {@link Tile tiles} and unique for a {@link it.polimi.ingsw.model.Session session},
+ * also contains the {@link it.polimi.ingsw.model.player.Worker workers} during the game
+ */
 public class Board {
 
     public static final int WIDTH = 5;
@@ -8,10 +11,18 @@ public class Board {
 
     private final static Tile[][] tiles = new Tile[Board.WIDTH][Board.HEIGHT];
 
+    /**
+     * Initializes the Board, it calls the method {@link #clear() clear}
+     *
+     */
     public Board() {
         clear();
     }
 
+    /**
+     * It replaces all the {@link Tile tiles} in the Board with new {@link Tile tiles}
+     *
+     */
     public void clear() {
         for (int x=0; x<Board.WIDTH; x++) {
             for (int y=0; y<Board.HEIGHT; y++) {
@@ -20,6 +31,12 @@ public class Board {
         }
     }
 
+    /**
+     * Getter for a specific {@link Tile tile} identified by its {@link Position position}
+     *
+     * @param position the position of the required {@link Tile tile}
+     * @return the {@link Tile tile} identified by the {@link Position position} 
+     */
     public Tile getTile(Position position) {
         return tiles[position.getX()][position.getY()]; }
 
