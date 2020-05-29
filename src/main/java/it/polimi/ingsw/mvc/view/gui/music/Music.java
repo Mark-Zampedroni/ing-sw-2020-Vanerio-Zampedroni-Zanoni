61d6b6798afae2c3d04812ef37d86c6928ce73ac
Stefano-Vanerio <61703922+Stefano-Vanerio@users.noreply.github.com>
2020-05-29 10:08:53 +0200

    Aggiunta musica di sottofondo

---
+++ src/main/java/it/polimi/ingsw/mvc/view/gui/music/Music.java
@@ -0,0 +1,30 @@
+package it.polimi.ingsw.mvc.view.gui.music;
+
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import java.io.File;
+
+public class Music {
+
+    public static void playMusic() {
+
+        try {
+            File music = new File("src/main/resources/music/musicname.wav");
+            AudioInputStream audio = AudioSystem.getAudioInputStream(music);
+            Clip clipper = AudioSystem.getClip();
+            System.out.println("Playing background music...");
+            clipper.open(audio);
+            clipper.start();
+            clipper.loop(Clip.LOOP_CONTINUOUSLY);
+
+        }
+        catch (Exception e) {
+            System.out.println("ERROR [MUSIC]");
+            e.printStackTrace();
+        }
+    }
+
+
+
+}
