d50236b2d4b9672c75ca844d1a8cfd0213ef6d9c
Marco-Zanoni <marco2.zanoni@mail.polimi.it>
2020-06-07 00:29:40 +0200

    God Selection controller

--- src/main/java/it/polimi/ingsw/mvc/view/gui/fxmlControllers/GodSelectionController.java
+++ src/main/java/it/polimi/ingsw/mvc/view/gui/fxmlControllers/GodSelectionController.java
@@ -10,6 +10,7 @@ import javafx.scene.control.Label;
 import javafx.scene.layout.Border;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
+import javafx.scene.text.TextAlignment;
 
 import java.util.*;
 
@@ -24,6 +25,9 @@ public class GodSelectionController extends GenericController {
         private GridPane firstGrid;
         private GridPane initGrid;
         private GridPane infoPowerGrid;
+        private GridPane preGod;
+
+        private String playerName;
 
         public GodCard(String godName) {
             initInit();
@@ -47,7 +51,7 @@ public class GodSelectionController extends GenericController {
 
         }
 
-        private void initDescription(String god){
+        private void initDescription(String godName){
             Label godNameLabel = new Label();
             Label descriptionLabel = new Label();
             Label conditionLabel = new Label();
@@ -57,36 +61,37 @@ public class GodSelectionController extends GenericController {
             initGrid.add(gridPreDescription,0,0);
             GridPane gridLabel = createGrid(16,10);
             gridPreDescription.add(gridLabel,0,0);
-            godNameLabel.setText(god);
+            godNameLabel.setText(godName);
             godNameLabel.getStyleClass().add("fullbackground");
             godNameLabel.setId("godNameLabel");
-            godNameLabel.setAlignment(Pos.CENTER);
             godNameLabel.setPrefSize(200,50);
-            String[] temp = Gods.valueOf(god).getDescription().split(":");
+            String[] temp = Gods.valueOf(godName).getDescription().split(":");
             conditionLabel.setText("      Effect\n ("+temp[0]+")");
-            conditionLabel.setAlignment(Pos.CENTER);
             conditionLabel.setPrefSize(200,100);
+            conditionLabel.setId("conditionLabel");
             descriptionLabel.setText(temp[1]);
-            descriptionLabel.setWrapText(true);
             descriptionLabel.setAlignment(Pos.TOP_LEFT);
             descriptionLabel.setPrefSize(200,200);
+            descriptionLabel.setId("descriptionLabel");
             gridLabel.add(godNameLabel,0,0,10,2);
             gridLabel.add(conditionLabel,0,2,10,3);
             gridLabel.add(descriptionLabel,0,5,10,9);
-
+            gridPreDescription.setOnMouseExited(event -> showNode(preGod));
 
         }
 
 
         private void initGodPane(String godName){
+            preGod = createGrid(5,1);
             god = new BorderPane();
             god.getStyleClass().add("fullbackground");
             god.setId(godName);
-            initGrid.add(god,0,0);
+            preGod.add(god,0,0,1,4);
+            initGrid.add(preGod,0,0);
             GridPane borderGod = createGrid(1,1);
             borderGod.getStyleClass().add("fullbackground");
             borderGod.setId("godBackground");
-            god.setCenter(borderGod);
+            preGod.add(borderGod,0,0,1,5);
             infoPowerGrid = createGrid(4,2);
             borderGod.add(infoPowerGrid,0,0);
         }
@@ -105,8 +110,7 @@ public class GodSelectionController extends GenericController {
             godPowerPane.setId(godPower);
             infoGrid.add(infoBorder,3,1,2,2);
             godPowerGrid.add(godPowerPane,1,0, 4, 6);
-            infoBorder.setOnMouseEntered(event -> toggleVisibility());
-            infoBorder.setOnMouseExited(event -> toggleVisibility());
+            infoBorder.setOnMouseEntered(event -> hideNode(preGod));
 
         }
 
@@ -120,29 +124,50 @@ public class GodSelectionController extends GenericController {
             actionPane.getStyleClass().add("invisiblebackground");
             actionPane.setId("selectbutton");
             actionText.setText("SELECT");
-            actionText.setPadding(new Insets(0,0,10,0));
+            actionText.setPadding(new Insets(0,0,7,0));
             actionButtonGrid.add(actionPane,1,1,5,4);
-
-        }
-
-        public void toggleVisibility(){
-                god.setVisible(!god.isVisible());
         }
 
         public String getGod() {return god.getId();}
 
-        public void disableActionButton(){
-            actionPane.setDisable(true);
-        }
+        public void disableActionButton(){actionPane.setDisable(true);}
 
         public void enableActionButton() {
-            actionPane.setDisable((false));
-            actionPane.setOnMouseEntered(event -> actionPane.setId("selectbuttonpressed"));
-            actionPane.setOnMouseExited(event -> actionPane.setId("selectbutton"));
+            if (!actionPane.getId().equals("playerNameLabel")) {
+                actionPane.setDisable((false));
+                actionPane.setOnMouseEntered(event -> actionPane.setId("selectbuttonpressed"));
+                actionPane.setOnMouseExited(event -> actionPane.setId("selectbutton"));
+                actionPane.setOnMouseReleased(event -> gui.validatePlayerGodChoice(getGod()));
+            }
         }
 
-        public boolean isSelect(){
-            return actionText.getText().equals("SELECT");
+        public String getPlayerName() {
+            return playerName;
+        }
+
+        public void setPlayerName(String playerName) {
+            this.playerName = playerName;
+            actionText.setText(playerName);
+            actionPane.setId("playerNameLabel");
+            actionText.setOpacity(1);
+            actionText.setPadding(new Insets(0,0,2,0));
+            // could setDisable
+        }
+
+        public void starterPlayer(){
+            actionPane.setDisable(false);
+            actionPane.setId("playerbutton");
+            actionText.setPadding(new Insets(0,0,7,0));
+            actionPane.setOnMouseEntered(event -> actionPane.setId("playerbuttonpressed"));
+            actionPane.setOnMouseExited(event -> actionPane.setId("playerbutton"));
+            actionPane.setOnMouseReleased(event -> gui.validatePlayer(getPlayerName()));
+
+        }
+
+        public void NotstarterPlayer(){
+            actionPane.setId("selectbutton");
+            actionText.setPadding(new Insets(0,0,7,0));
+            disableActionButton();
         }
     }
 
@@ -159,27 +184,19 @@ public class GodSelectionController extends GenericController {
     @FXML
     GridPane gridThreePlayer;
 
-    List<String> gods = new ArrayList<>();
-    Map<String, String> god = new HashMap<>();
+
     List<GodCard> cards = new ArrayList<>();
 
-   // validatePlayerGodChoice for choosing gods
-    // validatePlayer for starter player
 
     public void initialize() {
         super.initialize(this);
-        gods.add("APOLLO");
-        gods.add("ATHENA");
-        //gods.add("PAN");
-        requestPlayerGod(gods,god);
-        initTitleLabel();
-
-
     }
 
     public void updatePlayerGodSelection(String turnOwner, Map<String, String> choices, List<String> chosenGods) {
         // update da view quando è il turno di un altro client
         loadSelectedGods(chosenGods, choices);
+        titleLabel.setText(turnOwner + " is choosing a god");
+        for(GodCard gods : cards){gods.disableActionButton();}
 
     }
 
@@ -187,30 +204,34 @@ public class GodSelectionController extends GenericController {
     public void requestPlayerGod(List<String> chosenGods, Map<String, String> choices) {
         // update da view quando tocca a questo client
         loadSelectedGods(chosenGods, choices);
+        titleLabel.setText("Choose your god");
+        for(GodCard gods : cards){gods.enableActionButton();}
     }
 
 
-    public void updateStarterPlayerSelection(Map<String, String> choices) { //DIO,PLAYER
+    public void updateStarterPlayerSelection(Map<String, String> choices) {
         // update da view a tutti i giocatori notificando che il challenger sta scegliendo lo starter player
+        titleLabel.setText("The Challenger is choosing the starter player");
+        for(GodCard godCard: cards){godCard.NotstarterPlayer();}
     }
 
     public void requestStarterPlayer(Map<String, String> choices) {
         // update da view al challenger avvertendolo che deve scegliere lo starter player
+        titleLabel.setText("Choose the starter player");
+        for(GodCard godCard: cards){godCard.starterPlayer();}
     }
 
 
-    private void initTitleLabel() {
-        titleLabel.setText("Choose your god");
-    }
 
     private void loadSelectedGods(List<String> chosenGods, Map<String, String> choices) {
-       // if (chosenGods.size() == Integer.parseInt(gui.getNumberOfPlayers())) {
-            int i = 0;
+        if (chosenGods.size() == Integer.parseInt(gui.getNumberOfPlayers())) {
+        int i = 0;
             for (String god : chosenGods) {
                 GodCard newGodCard = new GodCard(god);
-                //if (Integer.parseInt(gui.getNumberOfPlayers()) == 3) {
-                if(chosenGods.size() == 3){
+                if (Integer.parseInt(gui.getNumberOfPlayers()) == 3) {
                     gridThreePlayer.add(newGodCard, i, 0);
+                    twoPlayerFirstGrid.setDisable(true);
+                    twoPlayerSecondGrid.setDisable(true);
                     i++;
                 } else {
                     if (i == 0) {
@@ -220,11 +241,15 @@ public class GodSelectionController extends GenericController {
                         twoPlayerSecondGrid.add(newGodCard, 0, 0);
                     }
                 }
+                cards.add(newGodCard);
             }
-       /* }
+         }
         else{
-            Update
-        }
-        */
+            choices.keySet().stream()
+                    .filter(nome -> !nome.equals(""))
+                    .forEach(key -> {for(GodCard god: cards){
+                        if(god.getGod().equals(choices.get(key))){god.setPlayerName(key);}
+                    }});
+            }
     }
 }
