421d6640f2736fe778414b25d50d09e993119254
Mark Zampedroni <10608097@polimi.it>
2020-06-10 02:15:37 +0200

    Sistemato scalabilità lobby

--- src/main/java/it/polimi/ingsw/mvc/view/gui/Gui.java
+++ src/main/java/it/polimi/ingsw/mvc/view/gui/Gui.java
@@ -29,7 +29,7 @@ public class Gui extends Application {
         stage.setScene(scene);
         //GuiManager.setLayout(stage.getScene(), GuiManager.getFxmlPath(TitleController.class)); // start screen
         Platform.runLater(() -> bindScene(stage.getScene()));
-        GuiManager.setLayout(stage.getScene(), GuiManager.getFxmlPath(BoardController.class));
+        GuiManager.setLayout(stage.getScene(), GuiManager.getFxmlPath(TitleController.class));
         //GuiManager.setLayout(stage.getScene(), GuiManager.getFxmlPath(BoardController.class)); // start screen
         stage.show();
         GuiManager.getInstance().setDefaultWidth(stage.getWidth());
