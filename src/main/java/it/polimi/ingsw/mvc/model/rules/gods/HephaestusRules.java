a40e28cb7b7f0e5e66f0badae4407ec74abbaa25
Stefano-Vanerio <61703922+Stefano-Vanerio@users.noreply.github.com>
2020-06-23 20:42:37 +0200

    Bug fix su efesto

--- src/main/java/it/polimi/ingsw/mvc/model/rules/gods/HephaestusRules.java
+++ src/main/java/it/polimi/ingsw/mvc/model/rules/gods/HephaestusRules.java
@@ -3,17 +3,49 @@ package it.polimi.ingsw.mvc.model.rules.gods;
 import it.polimi.ingsw.mvc.model.map.Position;
 import it.polimi.ingsw.mvc.model.player.Worker;
 import it.polimi.ingsw.mvc.model.rules.Check;
+import it.polimi.ingsw.mvc.model.rules.EventRules;
 import it.polimi.ingsw.utility.enumerations.Action;
 import it.polimi.ingsw.utility.exceptions.actions.CantActException;
 
 import java.io.Serializable;
+import java.util.List;
 
 /**
  * Rules for a player with Hephaestus as God
  */
-public class HephaestusRules extends DemeterRules implements Serializable {
+public class HephaestusRules extends EventRules implements Serializable {
 
     private static final long serialVersionUID = -1708343556340324002L;
+
+    /**
+     * Executes a {@link Action build}, if it is the first build of the turn saves
+     * the position
+     *
+     * @param position {@link Position position} where to build
+     */
+    @Override
+    public void executeBuild(Position position) {
+        if (!getEvent()) {
+            setPos(position);
+        }
+        super.executeBuild(position);
+    }
+
+    /**
+     * Returns a list of possible actions after a {@link Action build}, if it's the first build of the
+     * turn adds another {@link Action build}
+     *
+     * @return list of {@link Action actions} that can be done after {@link Action building}
+     */
+    @Override
+    public List<Action> afterBuild() {
+        List<Action> actions = super.afterBuild();
+        if (!getEvent()) {
+            actions.add(Action.BUILD);
+            setEvent(true);
+        }
+        return actions;
+    }
 
     /**
      * Checks if by the rules it's physically possible to perform a build {@link Action action},
