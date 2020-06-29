7c890531ccb2a7564d9ea29e100f0b5199cdbf4c
Marco-Zanoni <marco2.zanoni@mail.polimi.it>
2020-06-29 20:32:06 +0200

    Client connection comment

--- src/main/java/it/polimi/ingsw/network/client/ClientConnection.java
+++ src/main/java/it/polimi/ingsw/network/client/ClientConnection.java
@@ -77,6 +77,10 @@ public class ClientConnection implements Runnable {
         }
     }
 
+    /**
+     * Task of the connection Thread;
+     * Receives the messages and puts them in queue
+     */
     @Override
     public void run() {
         while (!Thread.currentThread().isInterrupted()) {
@@ -113,7 +117,7 @@ public class ClientConnection implements Runnable {
     }
 
     /**
-     * Disconnects a client
+     * Disconnects a connection
      */
     public void disconnect() {
         isDisconnected = true;
@@ -143,7 +147,7 @@ public class ClientConnection implements Runnable {
     }
 
     /**
-     * Sends a reconnection message
+     * Starts a reconnection request
      */
     private void startReconnectionRequests() {
         while (!reconnectRequest()) {
@@ -154,6 +158,11 @@ public class ClientConnection implements Runnable {
         }
     }
 
+    /**
+     * Checks if the reconnection request is successful
+     *
+     * @return {@code true} if it has started the reconnection
+     */
     private boolean reconnectRequest() {
         try {
             startConnection();
@@ -163,26 +172,52 @@ public class ClientConnection implements Runnable {
         }
     }
 
+    /**
+     * Checks if reconnecting
+     *
+     * @return {@code true} if reconnecting
+     */
     protected boolean getReconnect() {
         return reconnect;
     }
 
+    /**
+     * Setter for the reconnection
+     *
+     * @param value value for the reconnection
+     */
     protected void setReconnect(boolean value) {
         reconnect = value;
     }
 
+    /**
+     * Sets the name of the connection
+     *
+     * @param name name being set
+     */
     protected void setConnectionName(String name) {
         this.name = name;
     }
 
+    /**
+     * Checks if a connection is disconnected
+     *
+     * @return {@code true} if disconnected
+     */
     protected boolean isDisconnected() {
         return isDisconnected;
     }
 
+    /**
+     * Sets a connection as disconnected
+     */
     protected void setDisconnected() {
         this.isDisconnected = true;
     }
 
+    /**
+     * Puts the thread into a sleeping state
+     */
     private void waitTime() {
         try {
             Thread.sleep(2000);
